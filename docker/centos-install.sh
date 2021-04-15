b8e08c353d126a7d0ed285ef9ba180bab0429c11
haekyu.cho <haekyu.cho@nhn.com>
2021-04-15 19:00:17 +0900

    fix pro admin build script to be fast

--- docker/centos-install.sh
+++ docker/centos-install.sh
@@ -1,4 +1,4 @@
-cd ~
+sudo su -
 sudo yum -y install git
 sudo yum -y install zsh
 sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -7,10 +7,12 @@ sed -i "/ZSH_THEME=/c\ZSH_THEME=\"agnoster\"" ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-sed -i "/ZSH_THEME=/c\ZSH_THEME=\"agnoster\"" ~/.zshrc
-sed -i "/plugins=(git)/c\plugins=(\n\tgit\n\tzsh-syntax-highlighting\n\tzsh-autosuggestions\n)" ~/.zshrc
+sudo sed -i "/ZSH_THEME=/c\ZSH_THEME=\"agnoster\"" ~/.zshrc
+sudo sed -i "/plugins=(git)/c\plugins=(\n\tgit\n\tzsh-syntax-highlighting\n\tzsh-autosuggestions\n)" ~/.zshrc
 
 source ~/.zshrc
 
-# angoster
+# sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/chk386/kafka-connect-playground/master/docker/centos-install.sh)"
 
+
+# ssh -i notifications.pem centos@133.186.229.150
